931cd825a8c3770a243a1d737edf417501d35566
christophegragnic@yahoo.fr <christophegragnic@yahoo.fr@d12aad66-0f56-0410-8b2c-c7101ecc2a8f>
2009-10-27 08:18:28 +0000

    TABS to four spaces, removed some empty lines.
    git-svn-id: https://microlua.googlecode.com/svn@103 d12aad66-0f56-0410-8b2c-c7101ecc2a8f

--- trunk/lua-5.1.3/src/ds/vars.h
+++ trunk/lua-5.1.3/src/ds/vars.h
@@ -2,10 +2,11 @@
 #define SCREEN_DOWN 1
 #define SCREEN_BOTH 2
 
-#define assert(l, cond, text)	if(!(cond)){ luaL_error(l, text); return 0;}
+#define assert(l, cond, text)
+if(!(cond)){ luaL_error(l, text); return 0;}
 
-#define	LUA_RAM  0
-#define	LUA_VRAM  1
+#define LUA_RAM  0
+#define LUA_VRAM  1
 
 int SCREEN_UP_DISPLAY;
 int SCREEN_DOWN_DISPLAY;
@@ -13,12 +14,12 @@ int spaceBetweenScreens;
 int decale;
 
 typedef struct mapObj{
-	UL_IMAGE * image;
-	unsigned short * table;
-	int width, height;
-	int tileWidth, tileHeight;
-	int scrollX, scrollY;
-	int spaceX, spaceY;
+    UL_IMAGE * image;
+    unsigned short * table;
+    int width, height;
+    int tileWidth, tileHeight;
+    int scrollX, scrollY;
+    int spaceX, spaceY;
 } Map;
 
 
