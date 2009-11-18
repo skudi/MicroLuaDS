6ee3e6539545dde092afec4a62890b1cdee9c8ef
christophegragnic@yahoo.fr <christophegragnic@yahoo.fr@d12aad66-0f56-0410-8b2c-c7101ecc2a8f>
2009-11-18 17:05:23 +0000

    Changed tabs to spaces, removed commented code.
    git-svn-id: https://microlua.googlecode.com/svn@213 d12aad66-0f56-0410-8b2c-c7101ecc2a8f

--- trunk/source/main.c
+++ trunk/source/main.c
@@ -1,4 +1,4 @@
-#define COMPILE_FOR_DS 1	// 1: yes, 0: no$gba
+#define COMPILE_FOR_DS 1    // 1: yes, 0: no$gba
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,48 +15,45 @@
 
 int main()
 {
-	// Initialization of µlibrary
-	ulInit(UL_INIT_ALL);
-	ulInitGfx();
-	ulInitText();
+    // Initialization of µlibrary
+    ulInit(UL_INIT_ALL);
+    ulInitGfx();
+    ulInitText();
 
-	// Sucks banks C-D + drops at 30 fps
-	ulInitDualScreenMode();
-	
-	// Use bright pink as a transparent color
-	ulSetTransparentColor(RGB15(31, 0, 31));
+    // Sucks banks C-D + drops at 30 fps
+    ulInitDualScreenMode();
+    
+    // Use bright pink as a transparent color
+    ulSetTransparentColor(RGB15(31, 0, 31));
 
-	//ulDebug("Starting Lua...\n");
-	struct lua_State *l = lua_open();
-	if(!l)
-	{
-		ulDebug("\n\n\tFailed to create a Lua state - Push A to Exit\n");
-		return 0;
-	}
+    struct lua_State *l = lua_open();
+    if(!l)
+    {
+        ulDebug("\n\n\tFailed to create a Lua state - Push A to Exit\n");
+        return 0;
+    }
 
-	luaL_openlibs(l);
-	if(!COMPILE_FOR_DS){
-		if(luaL_loadfile(l,"boot.lua")){
-			ulDebug("Error Occured: Couldn't open boot.lua (tester version)\n");
-			return 0;
-		}
-	}else{
-		if(luaL_loadfile(l,"/lua/boot.lua")){
-			ulDebug("Error Occured: Couldn't open boot.lua\n");
-			return 0;
-		}
-	}
-	
-	//ulDebug("Lua started !\n");
-	
-	if(lua_pcall(l,0,0,0))
-	{
-		ulDebug(lua_tostring(l, -1));
-		lua_pop(l, 1);
-		return 0;
-	}
-	
-	lua_close(l);
+    luaL_openlibs(l);
+    if(!COMPILE_FOR_DS){
+        if(luaL_loadfile(l,"boot.lua")){
+            ulDebug("Error Occured: Couldn't open boot.lua (tester version)\n");
+            return 0;
+        }
+    } else {
+        if(luaL_loadfile(l,"/lua/boot.lua")){
+            ulDebug("Error Occured: Couldn't open boot.lua\n");
+            return 0;
+        }
+    }
+    
+    if(lua_pcall(l,0,0,0))
+    {
+        ulDebug(lua_tostring(l, -1));
+        lua_pop(l, 1);
+        return 0;
+    }
+    
+    lua_close(l);
 
-	return 0;
+    return 0;
 }
