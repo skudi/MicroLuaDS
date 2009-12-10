067000957ea3a2257699d63b36cb7400727ae260
Ced.le.pingouin <Ced.le.pingouin@d12aad66-0f56-0410-8b2c-c7101ecc2a8f>
2009-12-10 22:44:58 +0000

    Added a call to fatInitDefault() in main, removed code duplication when detecting if the libs have been loaded, and added some preprocessor commands to allow easier change of the base directory for the libs and the name of the main lua file to boot
    NOTE: COMPILE_FOR_DS is also set to zero in main (C code)
    
    WARNING: This commit and the previous one change the fat init code.
             It works for me with the latest devkitARM and libfat, I don't
             know if it works with the older dkA and libfat that we used to
             install.
             Also, more testing needs to be done to ensure that other fat 
             problems won't show up.
    
    
    git-svn-id: https://microlua.googlecode.com/svn@270 d12aad66-0f56-0410-8b2c-c7101ecc2a8f

--- trunk/source/main.c
+++ trunk/source/main.c
@@ -1,4 +1,13 @@
-#define COMPILE_FOR_DS 1    // 1: yes, 0: no$gba
+#define COMPILE_FOR_DS 0    // 1: yes, 0: no$gba
+
+#if COMPILE_FOR_DS
+#define BOOT_DIR "/lua/"
+#else
+#define BOOT_DIR ""
+#endif
+
+#define BOOT_FILE "libs.lua"
+#define BOOT_FULLPATH BOOT_DIR BOOT_FILE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,32 +34,26 @@ int main()
     
     // Use bright pink as a transparent color
     ulSetTransparentColor(RGB15(31, 0, 31));
-
+    
+    fatInitDefault();
+    
     struct lua_State *l = lua_open();
-    if(!l)
-    {
+    if (!l) {
         ulDebug("\n\n\tFailed to create a Lua state - Push A to Exit\n");
         return 0;
     }
-
+    
     luaL_openlibs(l);
-    if(!COMPILE_FOR_DS){
-        if(luaL_loadfile(l,"boot.lua")){
-            ulDebug("Error Occured: Couldn't open boot.lua (tester version)\n");
-            return 0;
-        }
-    } else {
-        if(luaL_loadfile(l,"/lua/boot.lua")){
-            ulDebug("Error Occured: Couldn't open boot.lua\n");
-            return 0;
-        }
+    if (luaL_loadfile(l, BOOT_FULLPATH)) {
+        ulDebug("Error Occured: Couldn't open %s (%s mode)\n", BOOT_FULLPATH, 
+                COMPILE_FOR_DS ? "DS" : "EMU");
+        return 0;
     }
     
     lua_pushliteral(l, "microlua 1");
     lua_setglobal(l, "_VERSION_DS");
     
-    if(lua_pcall(l,0,0,0))
-    {
+    if(lua_pcall(l,0,0,0)) {
         ulDebug(lua_tostring(l, -1));
         lua_pop(l, 1);
         return 0;
