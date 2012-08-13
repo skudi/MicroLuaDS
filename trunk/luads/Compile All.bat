aeeaddc2101ccc3d5c95c42129288c25b32b421f
mathieu@bacou.fr <mathieu@bacou.fr@d12aad66-0f56-0410-8b2c-c7101ecc2a8f>
2012-08-13 13:30:07 +0000

    Change compilation outputs.
    Errors for 'make' are directly displayed, while normal messages are still put in 'lua-5.1.3/out'.
    All the messages from 'make clean' and removal of some files in 'Compile DS.bat' are no longer displayed or saved, thus 'out2' is deleted.
    Don't forget to clean your repository before committing.
    
    git-svn-id: https://microlua.googlecode.com/svn@488 d12aad66-0f56-0410-8b2c-c7101ecc2a8f

--- trunk/luads/Compile All.bat
+++ trunk/luads/Compile All.bat
@@ -1,18 +1,18 @@
 @echo off
 
-echo Micro Lua DS compiler by Risike
+echo Micro Lua DS compiler by Risike [lualib]
 echo.
 
 cd lua-5.1.3
 echo Compiling Lualib...
-make clean 1> out
+make clean 1> nul 2>&1
 cd ..
-make clean 1> lua-5.1.3\out
+make clean 1> nul 2>&1
 cd lua-5.1.3/
 make 1> out
 echo.
 echo Installing Lualib...
-make install 1> out
+make install
 cd ..
 timeout /t 2 /nobreak > NUL
 
--- trunk/luads/Compile DS.bat
+++ trunk/luads/Compile DS.bat
@@ -1,17 +1,17 @@
 @echo off
 
-echo Micro Lua DS compiler by Risike
+echo Micro Lua DS compiler by Risike [Micro Lua]
 echo.
 
 echo Compiling Micro Lua DS...
 make
 
-del lua-5.1.3\src\*.o 2> lua-5.1.3\out2 1> lua-5.1.3\out
-del lua-5.1.3\src\*.a 2> lua-5.1.3\out2 1> lua-5.1.3\out
-del lua-5.1.3\src\ds\*.o 2> lua-5.1.3\out2 1> lua-5.1.3\out
-del lua-5.1.3\src\ds\*.a 2> lua-5.1.3\out2 1> lua-5.1.3\out
-del /F /S /Q build 1> lua-5.1.3\out 1> lua-5.1.3\out
-rmdir build 1> lua-5.1.3\out 1> lua-5.1.3\out
+del lua-5.1.3\src\*.o 1> nul 2>&1
+del lua-5.1.3\src\*.a 1> nul 2>&1
+del lua-5.1.3\src\ds\*.o 1> nul 2>&1
+del lua-5.1.3\src\ds\*.a 1> nul 2>&1
+del /F /S /Q build 1> nul 2>&1
+rmdir build 1> nul 2>&1
 
 echo Finished!
 
