9513247e3659f6c1de1083d07d82af244c01d738
Reylak <mathieu@bacou.fr>
2013-09-12 13:12:19 +0200

    Update README, add copyright terms.

--- source/main.c
+++ source/main.c
@@ -1,3 +1,22 @@
+/*  
+    Copyright 2008-2013 Risike, Reylak, MicroLua's developers
+    
+    This file is part of MicroLua.
+
+    MicroLua is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MicroLua is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MicroLua.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
